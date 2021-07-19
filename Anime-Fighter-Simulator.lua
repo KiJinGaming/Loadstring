913199d5cf16226273ee02967f60665f6169c8e1
KiJinGaming <84182232+KiJinGaming@users.noreply.github.com>
2021-07-19 17:25:45 +0700

    Update Anime-Fighter-Simulator.lua

--- Anime-Fighter-Simulator.lua
+++ Anime-Fighter-Simulator.lua
@@ -1,1 +1,8 @@
+--[[
+Feature:
+- Auto Farm
+- Auto Buy Star
+- Auto Fuse Pet (Level Lock)
+- ...
+--]]
 loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/KiJinGaming/FreeScript/main/AFterS.lua"))();
